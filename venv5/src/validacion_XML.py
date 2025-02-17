83da7b567b702bc668f6a4bf553d2174f16b8178
saenz03 <59660170+saenz03@users.noreply.github.com>
2025-02-17 10:02:08 -0500

    Delete venv5 directory

--- venv5/src/validacion_XML.py
+++
@@ -1,9 +0,0 @@
-import xml.etree.ElementTree as ET
-
-# Cargar el archivo XML
-tree = ET.parse(r"C:\Development\Habi\venv5\src\feed.xml")  # Reemplázalo con el nombre real del XML
-root = tree.getroot()
-
-# Mostrar los primeros elementos
-for elem in root[:1]:  # Mostrar solo los primeros 5 para revisar
-    print(ET.tostring(elem, encoding="utf-8").decode("utf-8"))
